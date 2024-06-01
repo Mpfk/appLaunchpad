b823bdca157f387b7eeb64618a142c20fc74bcb5
Matt Lydon <contact@mattlydon.me>
2024-06-01 00:57:00 -0400

    Output conflict fix

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,25 +1,33 @@
-/**
- * Outputs
- *
- * This file defines the outputs for the Terraform module. These outputs provide information about the CodeCommit repository and the Amplify app.
- */
+# This file defines the output variables for the Terraform module.
 
-output "repository_clone_url_http" {
-  description = "The HTTP clone URL of the CodeCommit repository"
+# Output: webapp_repository_clone_url_http
+# Description: The HTTP clone URL of the webApp CodeCommit repository
+# Value: module.webApp.repository_clone_url_http
+output "webapp_repository_clone_url_http" {
+  description = "The HTTP clone URL of the webApp CodeCommit repository"
   value       = module.webApp.repository_clone_url_http
 }
 
-output "repository_clone_url_ssh" {
-  description = "The SSH clone URL of the CodeCommit repository"
+# Output: webapp_repository_clone_url_ssh
+# Description: The SSH clone URL of the webApp CodeCommit repository
+# Value: module.webApp.repository_clone_url_ssh
+output "webapp_repository_clone_url_ssh" {
+  description = "The SSH clone URL of the webApp CodeCommit repository"
   value       = module.webApp.repository_clone_url_ssh
 }
 
-output "repository_name" {
-  description = "The name of the CodeCommit repository"
+# Output: webapp_repository_name
+# Description: The name of the webApp CodeCommit repository
+# Value: module.webApp.repository_name
+output "webapp_repository_name" {
+  description = "The name of the webApp CodeCommit repository"
   value       = module.webApp.repository_name
 }
 
-output "amplify_app_url" {
-  description = "The URL of the Amplify app"
+# Output: webapp_amplify_app_url
+# Description: The URL of the webApp Amplify app
+# Value: module.webApp.amplify_app_url
+output "webapp_amplify_app_url" {
+  description = "The URL of the webApp Amplify app"
   value       = module.webApp.amplify_app_url
 }
