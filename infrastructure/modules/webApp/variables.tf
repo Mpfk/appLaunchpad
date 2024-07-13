bc77b90fdc18e06bef003aef84ba92ec43ae8385
Matt Lydon <contact@mattlydon.me>
2024-07-13 16:32:02 -0400

    Resource Segmentation

--- infrastructure/modules/webApp/variables.tf
+++ infrastructure/modules/webApp/variables.tf
@@ -1,31 +1,6 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources..."
-  type        = string
-}
-
-variable "aws_profile" {
-  description = "The AWS CLI profile to use. Run `aws configure` or `aws sso configure` to set up a profile..."
-  type        = string
-}
-
-variable "project" {
-  description = "Name your project..."
-  type        = string
-}
-
-variable "custom_domain" {
-  description = "[OPTIONAL] Use a custom domain for the web application? Leave undefined if not using a custom domain. (example: mywebsite.com)"
-  type        = string
-}
-
-variable "prod_subdomain" {
-  description = "The subdomain for the production branch."
-  type        = string
-  default = ""
-}
-
-variable "dev_subdomain" {
-  description = "The subdomain for the development branch."
-  type        = string
-  default = "dev"
-}
+variable "aws_region"{}
+variable "aws_profile" {}
+variable "project" {}
+variable "custom_domain" {}
+variable "prod_subdomain" {}
+variable "dev_subdomain" {}
